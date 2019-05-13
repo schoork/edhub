30ddeab02c3e09380b9299a4e99fe0291131281c
Sam Williams <sblwilliams@gmail.com>
2019-05-13 16:00:34 -0500

    remove connectvars

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'mysql.sblwilliams.dreamhosters.com');
-  define('DB_USER', 'sblwilliams');
-  define('DB_PASSWORD', 'm1J8uSle81RD');
-  define('DB_NAME', 'sblwilliams_hollandale');
-?>
